bdad958f7440a198135e689710ae0969392678b7
ramzesmosiej <ramzesjunior9@gmail.com>
2022-10-09 17:24:43 +0200

    added kafka app

--- kafka-app/src/test/java/com/ramzescode/kafkaapp/KafkaAppApplicationTests.java
+++ kafka-app/src/test/java/com/ramzescode/kafkaapp/KafkaAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.kafkaapp;
+package com.ramzescode.kafkaapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KafkaAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
